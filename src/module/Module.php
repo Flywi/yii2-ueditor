d8f3aa1ef736608e251a2db4cd21e9106a56b115
Flywi <woflowin@gmail.com>
2018-07-26 17:25:19 +0800

    feat:结构调整

--- src/module/Module.php
+++ src/module/Module.php
@@ -6,21 +6,36 @@ use yii\base\Module as YiiModule;
 
 class Module extends YiiModule
 {
+    /**
+     * @var string
+     */
     public $controllerNamespace = 'Flywi\Editor\module\controllers';
 
+    /**
+     * @var string
+     */
     public $defaultRoute = 'index/index';
 
+    /**
+     * @var array
+     */
     public $actionMap = [
         'config' => '/editor/config/index',
         'uploadimage' => '/editor/upload/index'
     ];
 
+    /**
+     * init
+     */
     public function init()
     {
         $this->prepare();
         parent::init();
     }
 
+    /**
+     * prepare
+     */
     public function prepare(){
 
     }
