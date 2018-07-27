a3b13dd6e18e50fe2a6cb9a3779f1bfc5de2aed2
Flywi <woflowin@gmail.com>
2018-07-27 10:30:24 +0800

    fix:config bug

--- src/module/controllers/ConfigController.php
+++ src/module/controllers/ConfigController.php
@@ -2,15 +2,12 @@
 
 namespace Flywi\Editor\module\controllers;
 
-use Yii;
 use yii\web\Controller;
-use yii\web\Response;
 
 class ConfigController extends Controller
 {
     public function actionIndex()
     {
-        //Yii::$app->response->format = Response::FORMAT_JSON;
         $config = require_once(dirname(__DIR__) . '/config/ueditor.php');
         return json_encode($config);
     }
